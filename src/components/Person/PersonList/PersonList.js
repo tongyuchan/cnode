fda969e18e40f7c7747266579e2813594d71757d
tongyuchan <tongyuchan@leying365.com>
2017-08-05 23:57:23 +0800

    个人中心页面修改

--- src/components/Person/PersonList/PersonList.js
+++ src/components/Person/PersonList/PersonList.js
@@ -2,17 +2,60 @@ import React,{Component} from 'react';
 import './styles.scss';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {List,ListItem} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
 
 const PersonList=(props)=>{
     const {user}=props;
     return (
         <MuiThemeProvider>
             <div>
-                <div>
-                    <img src={user.avatar_url}/>
+                <div className="personTop">
+                    <div>
+                        <img src={user.avatar_url}/>
+                    </div>
+                    <p>{user.loginname}</p>
+                    <p>积分：{user.score}</p>
                 </div>
+                <List className="personList">
+                    <ListItem
+                        leftAvatar={<i className="iconfont icon-create"></i>}
+                        primaryText={
+                            <div>
+                                我创建的
+                                <span style={{float:'right'}} className="iconfont icon-right"></span>
+                            </div>
+                        }
+                    />
+                    <ListItem
+                        leftAvatar={<i className="iconfont icon-join"></i>}
+                        primaryText={
+                            <div>
+                                我参与的
+                                <span style={{float:'right'}} className="iconfont icon-right"></span>
+                            </div>
+                        }
+                    />
+                    <ListItem
+                        leftAvatar={<i className="iconfont icon-collect"></i>}
+                        primaryText={
+                            <div>
+                                我收藏的
+                                <span style={{float:'right'}} className="iconfont icon-right"></span>
+                            </div>
+                        }
+                    />
+                    <ListItem
+                        leftAvatar={<i className="iconfont icon-message"></i>}
+                        primaryText={
+                            <div>
+                                我的消息
+                                <span style={{float:'right'}} className="iconfont icon-right"></span>
+                            </div>
+                        }
+                    />
+                </List>
             </div>
-
         </MuiThemeProvider>
     )
 };
